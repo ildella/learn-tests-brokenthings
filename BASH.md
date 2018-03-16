c607f34890b30b090f219386e97dba22b13c7035
Daniele Dellafiore <ildella@gmail.com>
2018-03-16 18:15:45 +0000

    added sample file. The test is updated to try to parse the CSV in the stream without using any other library. Also because there are broken things: csv-stream fails for some reason, cat -A tells me the line separator is $ but in my code it works with \n... time to go to the pub

---
+++ BASH.md
@@ -0,0 +1,3 @@
+## Show text files as they are
+```cat -A file```
+This fill show the file with tabs, line breaks and non-printing characters
--- src/csvstream.js
+++ src/csvstream.js
@@ -15,7 +15,7 @@ class MockOutputStream extends stream.Writable {
       })
   }
   _write (chunk, enc, next) {
-    console.log(enc)
+    // console.log(enc)
     console.log(chunk)
   }
 }
@@ -25,19 +25,27 @@ async function process (context) {
   const csv = require('csv-stream')
   const options = {
     delimiter: ';', // default is ,
-    endLine: '\n', // default is \n,
+    endLine: '$', // default is \n,
     // columns: ['columnName1', 'columnName2'], // by default read the first line and use values found as columns
     columnOffset: 2, // default is 0
     escapeChar: '"', // default is an empty string
     enclosedChar: '"' // default is an empty string
   }
-  const inputStream = fs.createReadStream(context.source)
-  const outputStream = new MockOutputStream()
+  const readable = fs.createReadStream(context.source)
+  const writable = new MockOutputStream()
   const CSVStream = csv.createStream(options)
 
-  __(inputStream)
-    .through(CSVStream)
-    .pipe(outputStream)
+  readable.on('data', (chunk) => {
+    console.log(`Received ${chunk.length} bytes of data.`)
+    const stringChunk = chunk.toString('utf8')
+    const lines = stringChunk.split('\n')
+    console.log(lines)
+    console.log(lines.length)
+  })
+
+  // __(readable)
+  // .through(CSVStream)
+  // .pipe(writable)
 }
 
 module.exports.process = process
