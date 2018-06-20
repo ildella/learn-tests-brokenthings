53ea0b33af38b6b398307d12771ac8a60823bacd
Daniele Dellafiore <ildella@gmail.com>
2018-06-20 09:35:31 +0100

    cleanup the old gitignore I do not even remember where I took that from... Also upgraded reset script

--- reset.sh
+++ reset.sh
@@ -2,16 +2,17 @@
 echo 'Triacos - build automation since 2018'
 echo 'reset build environment'
 source build.conf
-git --version
-npm --version
-node --version
-# APP=worm-api-ci
-echo app name: $APP
-rm -rf ~/builds/$APP
+echo `heroku --version`
+[ -z "$DEV_APP" ] && echo "No DEV_APP, quitting" && exit 1
+[ -z "$PIPELINE" ] && echo "No PIPELINE, quitting" && exit 1
+echo app name: $DEV_APP
+rm -rf ~/builds/$DEV_APP
 heroku config:pull -f .env.pulled -a $DEV_APP
-heroku apps:destroy -a $APP --confirm $APP
-heroku apps:create $APP -t $TEAM --region eu
-heroku config:push -a $APP -o -c -f .env.pulled
+heroku apps:destroy -a $DEV_APP --confirm $DEV_APP
+heroku apps:create $DEV_APP -t worm --region eu --remote ci
+heroku addons:create rediscloud:30 -a worm-api-v3-dev
+heroku pipelines:add $PIPELINE -s development -a $DEV_APP
+heroku config:push -a $DEV_APP -o -c -f .env.pulled
 rm .env.pulled
 slack -c development -m "Build sandbox reset for: $APP" -u daniele
 echo 'Triacos wishes you an happy day'
