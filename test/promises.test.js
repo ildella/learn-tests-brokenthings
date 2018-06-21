e1bb465427f0539e9b69df285127464c2e11758d
Daniele Dellafiore <ildella@gmail.com>
2018-06-21 16:41:12 +0100

    specific test to have the cleanest implementation using await/async model

---
+++ test/promises.test.js
@@ -0,0 +1,29 @@
+const util = require('util')
+
+const job = async (param, cb) => {
+  console.log('starting...')
+  setTimeout(() => {
+    cb(null, `${param} - DONE`)
+  }, 100)
+}
+
+const promisifiedJob = util.promisify(job)
+
+const f1 = util.promisify(async (cb) => {
+  const result = await promisifiedJob(1)
+  cb(null, result)
+})
+
+test('2 nested promisified functions', async () => {
+  const result = await f1()
+  console.log(result)
+})
+
+test('test return with await with promisifiedJob', async () => {
+  console.log(await promisifiedJob(2))
+})
+
+// test('await does not work on non-promise, async () => {
+//   const result = await job(3)
+//   console.log(result)
+// })
