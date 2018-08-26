7c79b10378e58cc84a053e30e6ff41f1d28b2b9a
Daniele Dellafiore <ildella@gmail.com>
2018-08-26 13:27:58 +0100

    nice highland test

--- test/highland.test.js
+++ test/highland.test.js
@@ -1,62 +1,67 @@
+const axios = require('axios')
 const __ = require('highland')
 
-test('Highland', async () => {
-  const h = __([1, 2, 3, 4])
-  // .filter(undefined)
-  // .each(n => {
-  //   expect(n).toBeGreaterThan(0)
-  // })
-  expect(h.__HighlandStream__).toBeTruthy()
-  expect(h.append(5).__HighlandStream__).toBeTruthy()
-})
-
-test('Highland appen after map', async () => {
-  const h1 = __([1, 2, 3, 4])
-  const h2 = h1.map(n => { return 'a' })
-  expect(h1.__HighlandStream__).toBeTruthy()
-  expect(h1._already_consumed).toBeTruthy()
-  expect(h2._already_consumed).toBeFalsy()
-  // console.log(h2)
-  expect(h2.ended).toBeFalsy()
-  expect(h2.each(n => {}).ended).toBeTruthy()
-})
-
-test('just log error and move forward', async () => {
-  const s1 = __([1, 2, 3, 4])
-  expect(s1._incoming).toHaveLength(5)
-  const s2 = s1
+test('error chain', () => {
+  __([1, 2, 3, 4])
     .map(n => {
-      if (n >= 4) { throw new Error('too big') }
-      // if (n >= 2 && n < 4) { return 0 }
+      if (n > 3) {
+        throw new Error('more than 3')
+      }
       return n
     })
-    .errors((err, push) => {
-      console.log(err)
-      // push(null)
+    .map(n => {
+      console.log(n)
+      return n
     })
-  expect(s1.paused).toBeTruthy()
-  expect(s2.paused).toBeTruthy()
-  expect(s2.id).not.toEqual(s1.id)
-  s2
-    // .filter(item => item)
-    .each(n => console.log(n))
-    // .done(() => console.log('done'))
-  expect(s2.ended).toBeTruthy()
-  // console.log(s2)
+    .map(n => {
+      if (n > 2) {
+        throw new Error('more than 2')
+      }
+      return n
+    })
+    .errors(err => console.error(err.message))
+    .toArray(results => {
+      expect(results).toHaveLength(2)
+    })
 })
 
-test('fork', async () => {
-  const xs = __([1, 2, 3, 4])
-  const ys = xs.fork()
-  const zs = xs.fork()
+const remote = n => {
+  console.log(n)
+  if (n > 3) {
+    axios('http://localhost/unfedined')
+      .catch(error => {
+        // console.error(error)
+        throw new Error('buh!')
+      })
+  }
+  return n
+}
 
-  // no values will be pulled from xs until zs also resume
-  ys.resume()
+const remoteAsync = async n => {
+  console.log(n)
+  if (n > 2) {
+    const response = await axios('http://localhost/unfedined')
+  }
+  return n
+}
 
-  // now both ys and zs will get values from xs
-  zs.resume()
+test('axios', () => {
+  __([1, 2, 3, 4])
+    .map(__.wrapCallback(remote)).sequence()
+    .map(__.wrapCallback(remoteAsync)).sequence()
+    .errors(err => console.error(err.message))
+    .toArray(results => {
+      expect(results).toHaveLength(3)
+    })
 })
 
-test('collect errors in a different stream and do something else', async () => {
-  // TODO...
+test('through', () => {
+  __([1, 2, 3, 4])
+    .through(stream => {
+      expect(stream.__HighlandStream__).toBeTruthy()
+      return stream
+    })
+    .toArray(results => {
+      expect(results).toHaveLength(4)
+    })
 })
