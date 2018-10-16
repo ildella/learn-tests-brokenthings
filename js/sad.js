864a63172fea063e0d7da6a6c859ae4dfdca3177
Daniele Dellafiore <ildella@gmail.com>
2018-10-16 17:03:59 +0100

    because I do not understand regex and sed is too complicated for me :(

---
+++ js/sad.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+const append = (source, target) => {
+  const read = fs.createReadStream(source)
+  const write = fs.createWriteStream(target, {flags: 'a'})
+  read.pipe(write)
+}
+
+const overwrite = (source, target) => {
+  const read = fs.createReadStream(source)
+  const write = fs.createWriteStream(target, {flags: 'w'})
+  read.pipe(write)
+}
+
+append('./bash/bash_aliases', './output')
+overwrite('./bash/bash_aliases', './overwritten')
