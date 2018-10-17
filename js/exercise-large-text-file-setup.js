7094937f16684365466c3f4352d6cbade9da2651
Daniele Dellafiore <ildella@gmail.com>
2018-10-17 20:16:03 +0100

    ok well, what to say, maybe one should not do exercises like this after 7pm while drinking wine...

--- js/exercise-large-text-file-setup.js
+++ js/exercise-large-text-file-setup.js
@@ -8,17 +8,18 @@ const inputStream = fs.createReadStream('input-sample')
 // const outputStream = new stream()
 let counter = 0
 
-const rl = readline.createInterface({
-  input: inputStream,
-  // output: outputStream
-  output: process.stdout
-  // output: outputStream
-})
 inputStream.on('error', err => {
   console.error(err)
 })
 
-inputStream.on('line', line => {
+const rl = readline.createInterface({
+  input: inputStream,
+  // output: outputStream
+  // output: process.stdout
+  // output: outputStream
+})
+
+rl.on('line', line => {
   console.log('a')
   counter++
   // console.log('counter', counter)
@@ -31,7 +32,6 @@ rl.on('close', () => {
   process.exit(0)
 })
 
-// inputStream.pipe(outputStream)
 // inputStream.on('data', chunk => {
 //   console.log('data', chunk)
 // })
