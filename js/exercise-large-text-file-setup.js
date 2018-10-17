bfc666fb4653f97e71dc56c61233e8de8ec030cd
Daniele Dellafiore <ildella@gmail.com>
2018-10-17 20:22:50 +0100

    ok, the counter (that also write to another file the whole 2.5GB file) worked perfectly

--- js/exercise-large-text-file-setup.js
+++ js/exercise-large-text-file-setup.js
@@ -2,8 +2,9 @@ const fs = require('fs')
 const readline = require('readline')
 const stream = require('stream')
 
+const inputStream = fs.createReadStream('/home/ildella/Documents/exerciselargetext/itcont.txt')
 // const inputStream = fs.createReadStream('/home/ildella/Documents/exerciselargetext/by_date/itcont_2018_20180422_20180705.txt')
-const inputStream = fs.createReadStream('input-sample')
+// const inputStream = fs.createReadStream('input-sample')
 const outputStream = fs.createWriteStream('stream-output')
 // const outputStream = new stream()
 let counter = 0
@@ -20,11 +21,10 @@ const rl = readline.createInterface({
 })
 
 rl.on('line', line => {
-  console.log('a')
   counter++
   // console.log('counter', counter)
-  if(counter == 2) { rl.close() }
-  // outputStream.write(line)
+  // if(counter == 2) { rl.close() }
+  outputStream.write(line)
 })
 
 rl.on('close', () => {
