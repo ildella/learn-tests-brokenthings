a7c1042bbc4fa5dc2b33039558a7549679d5821c
Daniele Dellafiore <ildella@gmail.com>
2018-10-18 12:45:18 +0100

    simple example with 2 different JSON parsers used in highland

--- js/exercise-large-text-file-setup.js
+++ js/exercise-large-text-file-setup.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const readline = require('readline')
 const stream = require('stream')
 
-const inputStream = fs.createReadStream('/home/ildella/Documents/exerciselargetext/itcont.txt')
-// const inputStream = fs.createReadStream('/home/ildella/Documents/exerciselargetext/by_date/itcont_2018_20180422_20180705.txt')
+// const inputStream = fs.createReadStream('/home/ildella/Documents/exerciselargetext/itcont.txt')
+const inputStream = fs.createReadStream('/home/ildella/Documents/exerciselargetext/by_date/itcont_2018_20180422_20180705.txt')
 // const inputStream = fs.createReadStream('input-sample')
 const outputStream = fs.createWriteStream('stream-output')
 // const outputStream = new stream()
@@ -36,6 +36,10 @@ rl.on('close', () => {
   console.log('\nlines ->', counter)
   process.exit(0)
 })
+
+// expected:
+// ROOKE, JENNY
+// WASSIL, JAMES
 
 // inputStream.on('data', chunk => {
 //   console.log('data', chunk)
--- test/exercise-large-text-file.test.js
+++ test/exercise-large-text-file.test.js
@@ -12,27 +12,8 @@ process.on('unhandledRejection', (reason, p) => {
   console.error('Unhandled Rejection at', p, 'reason:', reason)
 })
 
-test('Create a small subset to test algos', async () => {
-  const inputStream = fs.createReadStream('/home/ildella/Documents/exerciselargetext/by_date/itcont_2018_20180422_20180705.txt')
-  const outputStream = fs.createWriteStream('sample')
-  const rl = readline.createInterface({
-    input: inputStream,
-    output: process.stdout
-  })
-  inputStream.on('error', err => {
-    console.error(err)
-  })
-  let counter = 0
-  inputStream.on('lines', line => {
-    outputStream.write(line)
-    counter++
-    if(counter === 2) { rl.close() }
-  })
-  rl.on('close', () => {
-    // console.log(wantedLines)
-    process.exit(0)
-  })
-  // inputStream.on('data', chunk => {
-  //   console.log('data', chunk)
-  // })
+const withHigland = require('../js/exercise-large-text-file-pipe')
+
+test('With Highland', async () => {
+  withHigland()
 })
