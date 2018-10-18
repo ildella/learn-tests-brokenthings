eeaf5f6c186ec7e2616098393f55494c5b41b1f3
Daniele Dellafiore <ildella@gmail.com>
2018-10-18 14:44:51 +0100

    removed pollution

--- js/exercise-large-text-file-readline.js
+++ js/exercise-large-text-file-readline.js
@@ -2,10 +2,9 @@ const fs = require('fs')
 const readline = require('readline')
 
 // const inputStream = fs.createReadStream('input-sample')
-// const inputStream = fs.createReadStream('data/itcont_2018_20180422_20180705.txt')
-const inputStream = fs.createReadStream('data/itcont.txt')
+const inputStream = fs.createReadStream('data/itcont_2018_20180422_20180705.txt')
+// const inputStream = fs.createReadStream('data/itcont.txt')
 const outputStream = fs.createWriteStream('readline-output')
-// const outputStream = process.stdout
 let counter = 0
 
 inputStream.on('error', err => {
@@ -26,12 +25,6 @@ rl.on('line', line => {
     outputStream.write(`${line.split('|')[7]}\n`)
   }
 })
-
-// expected:
-// MORTON GROOMS, KAREN VICTORIA
-// COLLINS, DARREN ROBERT
-// total lines -> 13709514
-// or                  13750541 ?
 
 rl.on('close', () => {
   console.log('\ntotal lines ->', counter)
