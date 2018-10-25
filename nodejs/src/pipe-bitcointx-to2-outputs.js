4b070da8e34d3f233cf8847099a4021e375fb963
Daniele Dellafiore <ildella@gmail.com>
2018-10-25 11:28:09 +0100

    completed the simplest posssible highland stream from rest apip to 2 local outputs

--- nodejs/src/pipe-bitcointx-to2-outputs.js
+++ nodejs/src/pipe-bitcointx-to2-outputs.js
@@ -1,4 +1,5 @@
 const tracer = require('tracer').colorConsole({level: process.env.LOG_LEVEL})
+const fs = require('fs')
 const __ = require('highland')
 const {DateTime} = require('luxon')
 // const {stringify} = require ('highland-json')
@@ -7,20 +8,26 @@ const api = require('axios').create({
   baseURL: 'https://blockchain.info/unconfirmed-transactions?format=json'
 })
 
-const hs = __((push, next) => {
+const mainStream = __((push, next) => {
   setTimeout(async () => {
     const response = await api.get()
-    console.log(`\n\n--- ${DateTime.local().toISO()} --- \n`)
+    // console.log(`\n\n--- ${DateTime.local().toISO()} --- \n`)
     push(null, response.data)
     next()
   }, 1000)
 })
   .map(data => data.txs).flatten()
-  .pick(['time', 'tx_index', 'hash'])
+  .pick(['time', 'tx_index', 'hash', 'weight'])
   // .map(json => {
   //   console.log(json)
   // })
   .through(JSONStream.stringify())
 
-hs.pipe(process.stdout)
+const ys = mainStream.fork()
+const zs = mainStream.observe()
+console.log('both streams will wait 2000ms before starting...')
+zs.pipe(fs.createWriteStream('bitcointx'))
+setTimeout(() => {
+  ys.pipe(process.stdout)
+}, 2000)
 // .done(() => console.log('DONE'))
