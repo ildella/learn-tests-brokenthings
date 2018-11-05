debbfa62784ceb184eec996f0e283bd6707eb066
Daniele Dellafiore <ildella@gmail.com>
2018-11-05 13:41:13 +0000

    updated implementation, almost ok still one strange bug...

--- nodejs/src/MongoStream.js
+++ nodejs/src/MongoStream.js
@@ -1,5 +1,5 @@
 const stream = require('stream')
-const tracer = require('tracer').colorConsole({level: process.env.LOG_LEVEL})
+const tracer = require('tracer').colorConsole({level: process.env.LOG_LEVEL || 'debug'})
 
 class MongoStream extends stream.Writable {
   constructor (db, collection, options={}) {
@@ -10,8 +10,9 @@ class MongoStream extends stream.Writable {
     this.db = db
     this.collection = collection
     this.name = `Mongo.${this.db.s.databaseName}.${this.collection}`
-    this.count = 0
+    this.inputCount = 0
     this.inserted_count = 0
+    this.options = options
     this.upsert = options.upsert || false
     self
       .on('error', (error) => {
@@ -22,21 +23,28 @@ class MongoStream extends stream.Writable {
   async _write (chunk, enc, next) {
     const isArray = Array.isArray(chunk)
     if (isArray) {
-      this.count += chunk.length
-      const result = await this.db.collection(this.collection).insertMany(chunk, { w: 1 })
-      tracer.trace(`inserted in ${this.collection}`, result.result)
-      this.inserted_count += result.result.n
+      this.inputCount += chunk.length
+      const response = await this.db.collection(this.collection).insertMany(chunk, this.options.write || {})
+      this.inserted_count += response.result.n
+      tracer.debug(`${this.collection}: ask to insert ${chunk.length} and inserted ${response.result.n}. Tot: ${this.inserted_count} out of ${this.inputCount}`)
       next()
-    } else if (this.upsert) {
-      this.count++
-      const result = await this.db.collection(this.collection).replaceOne({_id: chunk._id}, chunk, { upsert: true })
-      //TODO: writable.emit('info'...);
-      tracer.trace(`inserted in ${this.collection}`, result.result)
-      this.inserted_count += result.result.n
-      next()
+    // } else if (this.upsert) {
+    //   this.inputCount++
+    //   const response = await this.db.collection(this.collection).replaceOne({_id: chunk._id}, chunk, {upsert: true})
+    //   //TODO: writable.emit('info'...);
+    //   this.inserted_count += response.result.n
+    //   tracer.debug(`replaced document in ${this.collection}`, response.result)
+    //   next()
     } else {
-      this.count++
-      const result = await this.db.collection(this.collection).insertOne(chunk, { w: 1 }, next)
+      this.inputCount++
+      tracer.trace(this.options)
+      const response = await this.db.collection(this.collection).insertOne(chunk, this.options.write || {})
+      const inserted = response.result.n ? response.result.n : 0
+      tracer.debug(inserted)
+      this.inserted_count += inserted
+      tracer.debug
+      tracer.debug(`${this.collection}: ask to insert 1 and inserted ${response.result.n}. Session: inserted ${this.inserted_count} out of ${this.inputCount}`)
+      next()
     }
   }
 }
