fec7d3e570d48a25144ad3f6da407e7c825f0aca
Daniele Dellafiore <ildella@gmail.com>
2018-11-05 17:45:05 +0000

    now looks good

--- nodejs/test/promises.test.js
+++ nodejs/test/promises.test.js
@@ -1,22 +1,22 @@
 const {promisify} = require('util')
 
 const model = {
-  fetchAll: async (param, cb) => {
-    cb(null, {result: 'ok', param: param})
-  },
-
   info: () => {
     return 'I am what I am'
   },
 
-  fetch: async id => {
-    return {id: id, name: `${id}_name`}
+  fetch: async (id, cb) => {
+    cb(null, {id: id, name: `${id}_name`})
+  },
+
+  fetchAll: async (param, cb) => {
+    cb(null, {result: 'ok', param: param})
   }
+
 }
 
-const waitForIt = async (promise) => {
-  console.log('wait for it...', await promise)
-}
+const fetch = promisify(model.fetch)
+const fetchAll = promisify(model.fetchAll)
 
 const job = async (param, cb) => {
   console.log('starting...')
@@ -41,8 +41,6 @@ test('test return with await with promisifiedJob', async () => {
   console.log(await promisifiedJob(2))
 })
 
-const fetchAll = promisify(model.fetchAll)
-
 test('print the basics', async () => {
   console.log('the standard function')
   console.log(model.info)
@@ -53,18 +51,16 @@ test('print the basics', async () => {
   console.log('use Promise.all and we see the pending promises')
   console.log(Promise.all([model.fetchAll]))
   console.log(Promise.all([fetchAll]))
-
 })
 
-test('async functions promisify', async () => {
+test('async functions with callback, promisified', async () => {
   fetchAll('a')
     .then(response => console.log('boring old style...', response))
     .catch(err => console.error(err))
 
   const response = fetchAll('b')
   console.log('do not wait for it...', response)
-
-  waitForIt(response)
+  console.log('wait for it...', await response)
 
   Promise.all([fetchAll('c'), fetchAll('d')])
     .then(values => {
@@ -73,15 +69,9 @@ test('async functions promisify', async () => {
     })
 })
 
-test('good way to run multiple promises', async () => {
-  async function fetchFromIds (ids) {
-  // do NOT use await model.fetch(id) inside the map(). Instead:
-    const promises = ids.map(id => model.fetch(id))
-    return await Promise.all(promises)
-  }
-
-  const allItems = fetchFromIds([1, 2, 3])
-  waitForIt(allItems)
+test('good way to run multiple promises / avoid async in loops', async () => {
+  const promises = [5, 6, 7, 8].map(id => fetch(id))
+  console.log(await Promise.all(promises))
 })
 
 test('Function name and toStringTag', () => {
@@ -92,18 +82,19 @@ test('Function name and toStringTag', () => {
   expect(asyncFn[Symbol.toStringTag]).toEqual('AsyncFunction')
 })
 
-test('is Promise.promisifyAll() in yet?', async () => {
+test('how to build a Promise.promisifyAll() (ES2017)', async () => {
   const asyncFunctions = Object.values(model).filter(f => f[Symbol.toStringTag] === 'AsyncFunction')
   const syncFunctions = Object.values(model).filter(f => f[Symbol.toStringTag] !== 'AsyncFunction')
   const promisifiedAsyncFunctions = asyncFunctions.map(f => promisify(f))
-  console.log(asyncFunctions)
-  console.log(syncFunctions)
+  // console.log(asyncFunctions)
+  // console.log(syncFunctions)
   const promisifiedModel = {}
   promisifiedAsyncFunctions.forEach(f => promisifiedModel[f.name] = f)
   syncFunctions.forEach(f => promisifiedModel[f.name] = f)
-  console.log(model)
-  console.log(promisifiedModel)
-  console.log(promisifiedModel.fetch(99))
-  console.log(model.fetch(99))
+  // console.log(model)
+  // console.log(promisifiedModel)
+  // console.log('crash ->', model.fetch(99))
+  console.log('pending ->', promisifiedModel.fetch(99))
+  console.log('resolved async ->', await promisifiedModel.fetch(99))
   // const officialPromisifiedModel = Promise.promisifyAll(model)
 })
