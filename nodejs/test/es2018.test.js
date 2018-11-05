050d39a6b874666c55b3db31c5406b9f90bed988
Daniele Dellafiore <ildella@gmail.com>
2018-11-05 18:21:08 +0000

    ok, we're officially with ES2018 + basic samlpes of rest/spread new things

--- nodejs/test/es2018.test.js
+++ nodejs/test/es2018.test.js
@@ -22,3 +22,26 @@ test('good way to run multiple promises in ES2018', async () => {
   expect(values2).toHaveLength(0)
   expect(await Promise.all(promises)).toHaveLength(3)
 })
+
+test('rest/spread ES2015', async () => {
+  restParam(1, 2, 3, 4, 5)
+  function restParam (p1, p2, ...p3) {
+    expect(p1).toBe(1)
+    expect(p2).toBe(2)
+    expect(p3).toEqual([3, 4, 5])
+  }
+
+  const values = [99, 100, -1, 48, 16]
+  expect(Math.max(...values)).toBe(100)
+})
+
+test('rest/spread ES2018', async () => {
+  const myObject = {
+    a: 1,
+    b: 2,
+    c: 3
+  }
+  const {a, ...x} = myObject
+  expect(a).toBe(1)
+  expect(x).toEqual({b: 2, c: 3})
+})
