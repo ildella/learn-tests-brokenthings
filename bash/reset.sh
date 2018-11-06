f606345eb6ff621af2227f5bcd6bd4c05b4ba130
Daniele Dellafiore <ildella@gmail.com>
2018-11-06 10:12:56 +0000

    typo... old file I do not use really anymore but is nice to remember..

--- bash/reset.sh
+++ bash/reset.sh
@@ -9,8 +9,8 @@ echo app name: $DEV_APP
 rm -rf ~/builds/$DEV_APP
 heroku config:pull -f .env.pulled -a $DEV_APP
 heroku apps:destroy -a $DEV_APP --confirm $DEV_APP
-heroku apps:create $DEV_APP -t worm --region eu --remote ci
-heroku addons:create rediscloud:30 -a worm-api-v3-dev
+heroku apps:create $DEV_APP -t aTeamName --region eu --remote ci
+heroku addons:create rediscloud:30 -a $DEV_APP
 heroku pipelines:add $PIPELINE -s development -a $DEV_APP
 heroku config:push -a $DEV_APP -o -c -f .env.pulled
 rm .env.pulled
