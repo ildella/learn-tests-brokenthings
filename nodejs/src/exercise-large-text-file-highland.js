770928c577ee2862b96687f8fe2da358461d22ba
Daniele Dellafiore <ildella@gmail.com>
2018-11-08 14:50:17 +0000

    cleanup and setup for the 3rd question

--- nodejs/src/exercise-large-text-file-highland.js
+++ nodejs/src/exercise-large-text-file-highland.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const readline = require('readline')
 const __ = require('highland')
+// const stringStream = data => Buffer.from(data).toString()
 
-// const inputStream = fs.createReadStream('../data/input-sample')
+const inputStream = fs.createReadStream('../data/input-sample')
 // const inputStream = fs.createReadStream('../data/itcont_2018_20180422_20180705.txt')
-const inputStream = fs.createReadStream('../data/itcont.txt')
+// const inputStream = fs.createReadStream('../data/itcont.txt')
 // const outputStream = fs.createWriteStream('highland-output')
 
 const logger = item => {
@@ -12,43 +13,21 @@ const logger = item => {
   return item
 }
 
-const stringStream = data => Buffer.from(data).toString()
-const toLines = data => data.split(/\r?\n/)
-// const toLines = data => data.split(/\n/)
-const countWithCustomStringParser = () => {
-  let counter = 0
-  __(inputStream)
-    .map(stringStream)
-    .filter(string => string != undefined && string != null && string != '')
-    .map(toLines).flatten()
-    .filter(line => {
-      counter++
-      return true
-    })
-    .filter(line => {
-      return counter == 432 || counter == 43243
-    })
-    .map(line => line.split('|')[7])
-    .map(logger)
-    .done(() => console.log(`Real lines: 13709514. Counted lines -> ${counter}`))
-    // .pipe(outputStream)
-}
-
 const rl = readline.createInterface({input: inputStream})
-const countWithReadline = () => {
-  let counter = 0
-  const highlandStream = __((push, next) => {
-    rl.on('line', line => {
-      counter++
-      push(null, line)
-    })
-    rl.on('close', () => {
-      console.log(`total lines -> ${counter}`)
-      console.log(`used heap -> ${process.memoryUsage().heapTotal / 1024 / 1024}MB`)
-      console.log(`total heap -> ${process.memoryUsage().heapUsed / 1024 / 1024}MB`)
-    })
+let counter = 0
+const highlandStream = __((push, next) => {
+  rl.on('line', line => {
+    counter++
+    push(null, line)
   })
+  rl.on('close', () => {
+    console.log(`total lines -> ${counter}`)
+    console.log(`used heap -> ${process.memoryUsage().heapTotal / 1024 / 1024}MB`)
+    console.log(`total heap -> ${process.memoryUsage().heapUsed / 1024 / 1024}MB`)
+  })
+})
 
+const countWithReadline = () => {
   highlandStream
     .filter(line => {
       return counter == 432 || counter == 43243
@@ -59,14 +38,9 @@ const countWithReadline = () => {
 }
 
 const countDonationsPerMonth = () => {
-  const counter = 0
-  __(inputStream)
-    .map(stringStream)
-    .map(toLines).flatten()
-    // .map(line => line.split('|')[4])
-    // .filter(string => string != undefined && string != null && string != '')
-    // .map(date => date.substring(4, 6))
-    // .group(date => date.substring(4, 6))
+  highlandStream
+    .map(date => date.substring(4, 6))
+    .group(date => date.substring(4, 6))
     .group(line => line.split('|')[4].substring(4, 6))
     .map(item => {
       const month = Object.keys(item)[0]
@@ -79,4 +53,5 @@ const countDonationsPerMonth = () => {
     .done(() => console.log(`total lines -> ${counter}`))
 }
 
-countWithReadline()
+// countWithReadline()
+countDonationsPerMonth()
