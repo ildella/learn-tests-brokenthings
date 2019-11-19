e98b2bd07fc3e7a8e49d856166a994488df5ac13
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2019-11-19 17:11:39 +0100

    and now we have parallel remote transformations forked and merged together

--- nodejs/test/highland-advanced-errors-notifications.test.js
+++ nodejs/test/highland-advanced-errors-notifications.test.js
@@ -1,3 +1,4 @@
+jest.setTimeout(1000)
 const {empty} = require('ramda')
 const __ = require('highland')
 const {ObjectReadableMock, ObjectWritableMock} = require('stream-mock')
@@ -19,12 +20,15 @@ const reader = new ObjectReadableMock(input)
 const output = new ObjectWritableMock()
 const writeErrors = new ObjectWritableMock()
 const notifications = new ObjectWritableMock()
+const mergedNotifications = new ObjectWritableMock()
 output._write = (chunk, encoding, cb) => {
-  if (chunk.v > 4) {
+  console.log(chunk)
+  if (chunk.v > 4 || chunk.z > 6) {
     output.data.push(chunk)
     return cb()
   }
-  cb(`booooom - ${chunk.v}`)
+  // cb(`booooom - ${chunk.original}`)
+  cb(`booooom - `)
 }
 const writableStreamErrorSource = push => {
   output.on('error', err => {
@@ -52,29 +56,37 @@ const sourceStream = __(reader).ratelimit(1, 50)
 const writeErrorsStream = __(writableStreamErrorSource)
 const processingStream = sourceStream
   .filter(Number.isInteger)
-  .through(processingPipeline)
+  // .through(processingPipeline)
   // .sequence()
-  .errors(handleProcessingErrors)
+  // .errors(handleProcessingErrors)
 processingStream.observe().pipe(notifications)
+// const originalStream = processingStream.fork()
+const fetch1Stream = processingStream.fork().map(wrapPromise(fetch1))
+const fetch2Stream = processingStream.fork().map(wrapPromise(fetch2))
+const mergedStream = __([fetch1Stream, fetch2Stream]).merge().parallel(2)
 
 const sourceStreamInstrumentation = instrument(sourceStream)
 const processingStreamInstrumentation = instrument(processingStream)
 
 test('output stream error', done => {
   // expect.assertions(8)
-  writeErrors.on('finish', () => {
-    expect(writeErrors.data).toEqual(['booooom - 2', 'booooom - 4', 'booooom - 4'])
-    expect(writeErrors.writable).toBe(false)
-  })
   output.on('finish', () => {
     expect(handleProcessingErrors).not.toHaveBeenCalled()
     expect(sourceStreamInstrumentation.get()).toBe(7)
     expect(processingStreamInstrumentation.get()).toBe(5)
-    expect(notifications.data).toEqual([{v: 2}, {v: 4}, {v: 6}, {v: 16}, {v: 4}])
-    expect(output.data).toEqual([{v: 6}, {v: 16}])
+    expect(notifications.data).toEqual([1, 2, 3, 8, 2])
+    // expect(mergedNotifications.data).toEqual([{v: 2}, {z: 3}, {v: 4}, {z: 6}, {v: 6}, {z: 9}, {v: 16}, {z: 24}])
+    console.log(output.data)
+    expect(output.data).toEqual([{v: 6}, {z: 9}, {v: 16}, {z: 24}])
+    // expect(writeErrors.data).toEqual(['booooom - 2', 'booooom - 4', 'booooom - 4'])
+    expect(writeErrors.data).toEqual([
+      'booooom - ', 'booooom - ', 'booooom - ', 'booooom - ', 'booooom - ', 'booooom - '
+    ])
     expect(output.writable).toBe(false)
     done()
   })
   writeErrorsStream.pipe(writeErrors)
-  processingStream.pipe(output)
+  // processingStream.pipe(output)
+  mergedStream.pipe(output)
+  // originalStream.pipe(output)
 })
