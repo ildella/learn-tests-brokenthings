e61377cc16f3547bf1ef874826c1a86836b7ec8f
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2019-11-19 18:33:32 +0100

    added a new expected for the next improvement + reduced ratelimit window to 10 millisecs

--- nodejs/test/highland-advanced-errors-notifications.test.js
+++ nodejs/test/highland-advanced-errors-notifications.test.js
@@ -52,7 +52,7 @@ const transform = fetch => async item => {
   const response = await fetch(item)
   return {original: item, ...response}
 }
-const sourceStream = __(reader).ratelimit(1, 50)
+const sourceStream = __(reader).ratelimit(1, 10)
 const writeErrorsStream = __(writableStreamErrorSource)
 const processingStream = sourceStream
   .filter(Number.isInteger)
@@ -61,7 +61,7 @@ processingStream.observe().pipe(notifications)
 const fetch1Stream = processingStream.fork().map(wrapPromise(transform(fetch1)))
 const fetch2Stream = processingStream.fork().map(wrapPromise(transform(fetch2)))
 const mergedStream = __([fetch1Stream, fetch2Stream])
-  .ratelimit(1, 10).merge().parallel(2)
+  .ratelimit(1, 10).merge().parallel(3)
   // .tap(() => console.log(DateTime.local().c))
 
 const sourceStreamInstrumentation = instrument(sourceStream)
@@ -74,11 +74,14 @@ test('output stream error', done => {
     expect(sourceStreamInstrumentation.get()).toBe(7)
     expect(processingStreamInstrumentation.get()).toBe(5)
     expect(notifications.data).toEqual([1, 2, 3, 8, 2])
-    // expect(mergedNotifications.data).toEqual([{v: 2}, {z: 3}, {v: 4}, {z: 6}, {v: 6}, {z: 9}, {v: 16}, {z: 24}])
     expect(output.data).toEqual([
       {original: 3, v: 6}, {original: 3, z: 9},
       {original: 8, v: 16}, {original: 8, z: 24}
     ])
+    // expect(output.data).toEqual([
+    //   {original: 3, v: 6}, {original: 3, z: 9}, {original: 3, v: 6, z: 9}
+    //   {original: 8, v: 16}, {original: 8, z: 24}, {original: 8, v: 16, z: 24}
+    // ])
     expect(writeErrors.data).toEqual([
       'booooom - 1', 'booooom - 1', 'booooom - 2', 'booooom - 2', 'booooom - 2', 'booooom - 2'
     ])
