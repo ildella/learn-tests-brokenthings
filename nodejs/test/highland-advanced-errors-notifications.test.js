e71bcfc4ee2e7c7a8a7b742c6e0651a13bf186db
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2019-11-19 18:27:53 +0100

    now we have what we want but misses: 1) merge the object with same original together and 2) throw error in the source reader

--- nodejs/test/highland-advanced-errors-notifications.test.js
+++ nodejs/test/highland-advanced-errors-notifications.test.js
@@ -23,12 +23,13 @@ const writeErrors = new ObjectWritableMock()
 const notifications = new ObjectWritableMock()
 // const mergedNotifications = new ObjectWritableMock()
 output._write = (chunk, encoding, cb) => {
-  if (chunk.original >= 3 || chunk.z > 6) {
+  if (!chunk.original) cb('noooooo')
+  if (chunk.original >= 3) {
     output.data.push(chunk)
     return cb()
   }
-  // cb(`booooom - ${chunk.original}`)
-  cb(`booooom - `)
+  cb(`booooom - ${chunk.original}`)
+  // cb(`booooom - `)
 }
 const writableStreamErrorSource = push => {
   output.on('error', err => {
@@ -58,7 +59,7 @@ const processingStream = sourceStream
   .errors(handleProcessingErrors)
 processingStream.observe().pipe(notifications)
 const fetch1Stream = processingStream.fork().map(wrapPromise(transform(fetch1)))
-const fetch2Stream = processingStream.fork().map(wrapPromise(fetch2))
+const fetch2Stream = processingStream.fork().map(wrapPromise(transform(fetch2)))
 const mergedStream = __([fetch1Stream, fetch2Stream])
   .ratelimit(1, 10).merge().parallel(2)
   // .tap(() => console.log(DateTime.local().c))
@@ -74,11 +75,14 @@ test('output stream error', done => {
     expect(processingStreamInstrumentation.get()).toBe(5)
     expect(notifications.data).toEqual([1, 2, 3, 8, 2])
     // expect(mergedNotifications.data).toEqual([{v: 2}, {z: 3}, {v: 4}, {z: 6}, {v: 6}, {z: 9}, {v: 16}, {z: 24}])
-    expect(output.data).toEqual([{original: 3, v: 6}, {z: 9}, {original: 8, v: 16}, {z: 24}])
-    // expect(writeErrors.data).toEqual(['booooom - 2', 'booooom - 4', 'booooom - 4'])
-    expect(writeErrors.data).toEqual([
-      'booooom - ', 'booooom - ', 'booooom - ', 'booooom - ', 'booooom - ', 'booooom - '
+    expect(output.data).toEqual([
+      {original: 3, v: 6}, {original: 3, z: 9},
+      {original: 8, v: 16}, {original: 8, z: 24}
     ])
+    expect(writeErrors.data).toEqual([
+      'booooom - 1', 'booooom - 1', 'booooom - 2', 'booooom - 2', 'booooom - 2', 'booooom - 2'
+    ])
+    expect(writeErrors.data.length + output.data.length).toBe(input.length)
     expect(output.writable).toBe(false)
     done()
   })
