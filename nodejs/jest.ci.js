e1b0ab47a21c0e6f3e44b27f206efdf5235576dc
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2019-11-25 17:55:11 +0100

    polished jest config: remove useless ci only config and added the coverage npm script

--- nodejs/jest.ci.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  verbose: true,
-  notify: true,
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)(spec|test).js?(x)'],
-  collectCoverage: true,
-  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html', 'json-summary']
-}
--- nodejs/jest.config.js
+++ nodejs/jest.config.js
@@ -1,7 +1,20 @@
 module.exports = {
+  testEnvironment: 'node',
   verbose: false,
   notify: false,
-  testEnvironment: 'node',
+  notifyMode: 'failure-change, success-change',
   testMatch: ['**/?(*.)(spec|test).js?(x)'],
-  collectCoverage: false
+  // testMatch: ['**/tests/**/*.test.js'],
+  testPathIgnorePatterns: ['/node_modules'],
+  setupFilesAfterEnv: ['./jest-helpers.js'],
+  collectCoverage: false,
+  collectCoverageFrom: [
+    // '**/src/*.{js,jsx}',
+    '**/src/**/*.{js,jsx}',
+    '!**/node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {branches: 80, functions: 80, lines: 80, statements: 80}
+  },
+  coverageReporters: ['text', 'text-summary', 'json', 'json-summary', 'lcov', 'clover', 'html'],
 }
